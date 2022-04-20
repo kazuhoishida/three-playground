1221ae8aca568d90af20911edadf9ed70993e80b
Kazuho Ishida <k10700110@gmail.com>
2022-04-20 22:24:00 +0900

    add tailwind & make the chair two

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Suspense } from "react"
 import "./css/App.css"
 import { Canvas } from "@react-three/fiber"
-import { PerspectiveCamera, OrbitControls, Stage } from "@react-three/drei"
+import { PerspectiveCamera, OrbitControls, Stage, Html } from "@react-three/drei"
 import Chair from "./components/Chair"
 
 function Light() {
@@ -13,28 +13,35 @@ function Light() {
   )
 }
 
-function Section({ model, position }) {
+function Section({ model, scale, position }) {
   return (
-    <Stage>
-      <Chair scale={[1, 1, 1]} position={position} modelPath={model} />
-    </Stage>
+    <>
+      {/* 
+      <Stage>
+        <Chair scale={[1, 1, 1]} position={position} modelPath={model} />
+        <Chair scale={[1, 1, 1]} position={position} modelPath={model} />
+      </Stage> */}
+      <Html>
+        <h2 className="w-[200px] text-center -translate-x-1/2 text-gray font-bold">Chair Title</h2>
+      </Html>
+      <Chair scale={scale} position={position} modelPath={model} />
+    </>
   )
 }
 
 export default function App() {
   return (
     <div className="App">
-      <h1>chair model show</h1>
-      <div className="wrapper" style={{ width: "100vw", height: "100vh" }}>
-        <Suspense fallback={<p>loading now...</p>}>
-          <Canvas>
-            {/* <PerspectiveCamera makeDefault /> */}
-            <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
-            <Light />
-            <Section model={`src/assets/models/black_leather_chair.gltf`} position={[0, 0, 0]} />
-          </Canvas>
-        </Suspense>
-      </div>
+      <h1 className="text-center font-bold py-2">chair model show</h1>
+      <Suspense fallback={<p className="text-center">loading now...</p>}>
+        <Canvas className="bg-gray-300" style={{ width: "100vw", height: "50vh" }}>
+          {/* <PerspectiveCamera makeDefault /> */}
+          <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
+          <Light />
+          <Section model={`src/assets/models/black_leather_chair.gltf`} scale={[1, 1, 1]} position={[-0.5, 0, 1]} />
+          <Section model={`src/assets/models/black_leather_chair.gltf`} scale={[1, 1, 1]} position={[0.5, 0, 1]} />
+        </Canvas>
+      </Suspense>
     </div>
   )
 }
