d4081c2b5bcf594bd314ef56761de709aed8d000
Kazuho Ishida <k10700110@gmail.com>
2022-05-05 18:22:22 +0900

    separate astronaut page

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import Layout from "./components/Layout"
 import FirstView from "./components/FirstView"
 import Section1 from "./components/Section1"
 import Section2 from "./components/Section2"
-import EnvironmentSection from "./components/EnvironmentSection"
-import MovingLight from "./components/MovingLight"
 import SwitchTexture from "./components/SwitchTexture"
 
 export default function App() {
@@ -14,8 +12,6 @@ export default function App() {
         <FirstView />
         <Section1 />
         <Section2 />
-        <EnvironmentSection />
-        <MovingLight />
         <SwitchTexture />
       </div>
     </Layout>
--- src/components/EnvironmentSection.jsx
+++ src/components/EnvironmentSection.jsx
@@ -1,54 +1,59 @@
 import { Suspense, useState, useRef, useEffect } from "react"
 import { Canvas, useFrame } from "@react-three/fiber"
-import { PerspectiveCamera, OrbitControls, Environment, Stars, Html } from "@react-three/drei"
-import { EffectComposer, DepthOfField, Bloom } from "@react-three/postprocessing"
+import { PerspectiveCamera, OrbitControls, Environment, Stars } from "@react-three/drei"
+import { EffectComposer, SelectiveBloom } from "@react-three/postprocessing"
+import { BlurPass, Resizer, KernelSize } from "postprocessing"
 import Astronaut from "./Astronaut"
 
-function MoonMesh() {
+function CanvasSection({ env, star }) {
+  const envPresets = ["night", "sunset", "dawn"]
+  const moonRef = useRef()
+  const lightRef1 = useRef()
+  const lightRef2 = useRef()
+
   const r = 1.5
   const dTheta = (2 * Math.PI) / 1000
   const [theta, setTheta] = useState(0)
-
-  const ref = useRef()
 
   useFrame(() => {
     setTheta(theta + dTheta)
   })
 
   useEffect(() => {
-    ref.current.position.x = r * Math.cos(theta)
-    ref.current.position.z = r * Math.sin(theta)
+    moonRef.current.position.x = r * Math.cos(theta)
+    moonRef.current.position.z = r * Math.sin(theta)
   }, [theta])
   return (
-    <group>
-      <pointLight intensity={3} color="rgb(250, 200, 0)" />
-      <mesh position={[-1, 1, 1]} ref={ref}>
-        <pointLight intensity={2} color="rgb(250, 200, 0)" />
-        <sphereGeometry args={[0.2, 32, 32]} rotation={[0, 0.8, 0]} />
-        <meshStandardMaterial color="rgb(250, 200, 0)" />
-      </mesh>
-    </group>
-  )
-}
-
-function CanvasSection({ env, star }) {
-  const envPresets = ["night", "sunset", "dawn"]
-
-  return (
     <>
-      <Canvas className="w-full h-full">
-        <PerspectiveCamera makeDefault position={[3, 0.5, 0]} zoom={1} />
-        <OrbitControls enablePan={true} enableZoom={false} enableRotate={true} />
-        {/* <Light position={[10, 10, 10]} /> */}
-        <MoonMesh />
-        <Astronaut position={[0, -1, 0]} scale={[0.8, 0.8, 0.8]} rotation={[0, Math.PI / 3, 0]} />
-        {star && <Stars radius={100} depth={100} count={5000} factor={10} saturation={0} fade speed={1} />}
-        <Environment background={true} preset={envPresets[env]} />
+      <PerspectiveCamera makeDefault position={[2.8, 0.5, 0]} zoom={1} />
+      <OrbitControls enablePan={true} enableZoom={false} enableRotate={true} />
+      <ambientLight intensity={0.5} color="rgb(100, 100, 200)" />
+      <group>
+        <pointLight intensity={4} color="rgb(250, 200, 0)" ref={lightRef1} />
+        <mesh position={[-1, 1, 1]} ref={moonRef}>
+          <pointLight intensity={3} color="rgb(250, 200, 0)" ref={lightRef2} />
+          <sphereGeometry args={[0.2, 32, 32]} rotation={[0, 0.8, 0]} />
+          <meshStandardMaterial color="rgb(250, 200, 0)" />
+        </mesh>
+      </group>
+      <Astronaut position={[0, -1, 0]} scale={[0.8, 0.8, 0.8]} rotation={[0, Math.PI / 3, 0]} />
+      {star && <Stars radius={100} depth={100} count={5000} factor={10} saturation={0} fade speed={1} />}
+      <Environment background={true} preset={envPresets[env]} />
 
-        <EffectComposer>
-          <Bloom luminanceThreshold={0} luminanceSmoothing={0.9} height={900} />
-        </EffectComposer>
-      </Canvas>
+      <EffectComposer>
+        <SelectiveBloom
+          lights={[lightRef1, lightRef2]} // ⚠️ REQUIRED! all relevant lights
+          selection={[moonRef]} // selection of objects that will have bloom effect
+          selectionLayer={10} // selection layer
+          intensity={2.0} // The bloom intensity.
+          blurPass={undefined} // A blur pass.
+          width={Resizer.AUTO_SIZE} // render width
+          height={Resizer.AUTO_SIZE} // render height
+          kernelSize={KernelSize.LARGE} // blur kernel size
+          luminanceThreshold={0.05} // luminance threshold. Raise this value to mask out darker elements in the scene.
+          luminanceSmoothing={0.025} // smoothness of the luminance threshold. Range is [0, 1]
+        />
+      </EffectComposer>
     </>
   )
 }
@@ -58,11 +63,7 @@ export default function EnvironmentSection() {
   const [isShowStar, setIsShowStar] = useState(false)
 
   const changeEnv = () => {
-    if (envScene === 2) {
-      setEnvScene(0)
-    } else {
-      setEnvScene(envScene + 1)
-    }
+    envScene === 2 ? setEnvScene(0) : setEnvScene(envScene + 1)
   }
 
   return (
@@ -77,7 +78,9 @@ export default function EnvironmentSection() {
       </div>
       <Suspense fallback={<p className="text-center">loading now...</p>}>
         <div className="w-full h-full">
-          <CanvasSection env={envScene} star={isShowStar} />
+          <Canvas className="w-full h-full">
+            <CanvasSection env={envScene} star={isShowStar} />
+          </Canvas>
         </div>
       </Suspense>
     </div>
