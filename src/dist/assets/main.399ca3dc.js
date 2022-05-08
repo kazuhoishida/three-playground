3783be2ed069838300056bdbbeafaa442502810a
Kazuho Ishida <k10700110@gmail.com>
2022-05-08 20:45:08 +0900

    update vite config & delete dist dir

--- src/dist/assets/main.399ca3dc.js
+++
@@ -1,1 +0,0 @@
-import{j as t,L as e,c as r,R as o}from"./index.aa969110.js";function a(){return t(e,{})}r.createRoot(document.getElementById("root")).render(t(o.StrictMode,{children:t(a,{})}));
--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
   publicDir: "models",
   assetsInclude: ["**/*.glb", "**/*.gltf"],
   build: {
+    outDir: "../dist",
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
